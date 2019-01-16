41af5e9578cb4bc08cbe4528043dd6c3a0384cde
Michael Kornblum <mkornblum71@gmail.com>
2019-01-16 12:06:22 -0600

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+const user = {
+    name: 'John Doe',
+    address: {
+        street: '123 Main Street',
+        city: 'Portland',
+        state: 'Oregon',
+        zip: '5555'
+    },
+    phone: '555-555-5555',
+    zip: '55555'
+}
